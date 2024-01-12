aab5dec7ca5b005f73db477abefc9da4153d23b1
Rafi Sharkar <rafisharkar144@gmail.com>
2024-01-13 01:06:37 +0600

    Add infinity Slider of Service page

--- src/web/Pages/Services/Components/HeaderDev.jsx
+++ src/web/Pages/Services/Components/HeaderDev.jsx
@@ -1,14 +1,16 @@
+import style from './HeaderDev.module.css'
+
 export default function HeaderDev({title, subtitle, disc, img}) {
   return (
-    <div className='w-[100%] h-[70vh] flex'>
-    <div className='w-[50%] p-9'>
-      <h3>We are provide 1</h3>
-      <h1 className='text-[2.5rem] font-[700]'>{title}</h1>
-      <h2 className='text-[1.4rem] mb-4'>{subtitle}</h2>
+    <div className={style.mdiv}>
+    <div className={style.headertext}>
+      <h3>We are providing</h3>
+      <h1 className=''>{title}</h1>
+      <h2 className=''>{subtitle}</h2>
       <p>{disc}</p>
-      <button className='mt-8 border-[4px] rounded-2xl px-7 py-2 text-[1.5rem] font-[700] tracking-wide text-[green] hover:bg-[green] hover:text-[white]'>get start</button>
+      <button className={style.headerbtn}>Get start</button>
     </div>
-    <div className='w-[50%] bg-[#F5EE99]'>
+    <div className={style.headerimg}>
       <img src={img} alt="Image is not found" />
     </div>
   </div>
--- src/web/Pages/Services/Services.jsx
+++ src/web/Pages/Services/Services.jsx
@@ -1,12 +1,14 @@
 import HeaderSlider from "./Components/HeaderSlider";
 import ContactForm from '../../GlobalComponents/ContactForm'
 import Footer from '../../GlobalComponents/Footer'
+import InfinitySlider from "./Components/InfinitySlider";
 
 export default function Services() {
   return (
     <>
       <div className="relative z-[100]">
         <HeaderSlider/>
+        <InfinitySlider/>
         <ContactForm />
         <Footer />
       </div>
