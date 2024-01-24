006d9de879bc1073dd35d372a823ed8da7b9d485
Md. Ismail Hosen <ismailhosen1006@gmail.com>
2024-01-24 14:21:41 +0600

    terms and privacy policy page added

---
+++ src/web/GlobalComponents/Privacy_policy.jsx
@@ -0,0 +1,162 @@
+import React from "react";
+import Footer from "../GlobalComponents/Footer"
+export default function Privacy_policy() {
+  return (
+    <div className="w-[100%] flex flex-col justify-center items-start">
+      <div className="w-[100%] relative flex justify-center items-center">
+        <div className="w-[100%] absolute top-0 left-0 z-[10]">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path
+              fill="#F3F3F6"
+              fill-opacity="1"
+              d="M0,288L48,282.7C96,277,192,267,288,240C384,213,480,171,576,138.7C672,107,768,85,864,106.7C960,128,1056,192,1152,197.3C1248,203,1344,149,1392,122.7L1440,96L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+            ></path>
+          </svg>
+        </div>
+        <div className="w-[85%] relative z-[100] pt-[20px] md:pt-[25px] lg:pt-[80px]">
+          <div>
+            <h2 className="text-[#18B898] text-[20px]">Our Agency</h2>
+            <h1 className="text-[25px] text-[#FF4935]">PRIVACY POLICY</h1>
+          </div>
+          <div className="pt-[30px] flex flex-col justify-start items-start gap-[25px] md:pt-[45px]">
+            <div>
+              <h2 className="text-[#0A1D56] text-[30px] font-[500] pb-[5px]">Your Information</h2>
+              <div className="flex flex-col justify-start items-start gap-[10px]">
+                <p>
+                  Your privacy is very important to us. This Privacy Policy
+                  describes how your personal information is collected, used,
+                  and shared.
+                </p>
+                <p>
+                  This page is describing the types of personal information is
+                  gathered and recorded and how we use it.
+                </p>
+              </div>
+            </div>
+            <div>
+              <h2 className="text-[#0A1D56] text-[30px] font-[500] pb-[5px]">
+                Information we collect
+              </h2>
+              <div className="flex flex-col justify-start items-start gap-[10px]">
+                <p>
+                  <span className="text-[#18B898]">ouragency.com</span> and its
+                  operational service partners collect and use your personal
+                  data to process product orders and deliver the products as per
+                  requested.
+                </p>
+                <p>
+                  It is also possible that
+                  <span className="text-[#18B898]">ouragency</span> may also use
+                  personal information to inform you of other products or
+                  services available from{" "}
+                  <span className="text-[#18B898]">ouragency</span>
+                  and its affiliates. This may include your personal information
+                  such as name, home address, phone number, e-mail addresses and
+                  payment details (credit or debit card). If you need support
+                  our staff may need to know information to solve the problem
+                  such as operating system, browser software and editing
+                  software. When you visit this website we do record certain
+                  information using Google Analytics such as your location,
+                  browser version, ip address, screen resolution, operating
+                  system and flash support
+                </p>
+                <p>
+                  <span className="text-[#18B898]">ouragency</span> does not
+                  sell, rent or lease its customer lists to third parties.
+                  Standsweb may, from time to time, contact you on behalf of
+                  external business partners about a particular offering that
+                  may be of interest to you. In those cases, your personal
+                  information (e-mail, name, address, telephone number) is not
+                  transferred to the third party.
+                </p>
+              </div>
+            </div>
+            <div>
+              <h2 className="text-[#0A1D56] text-[30px] font-[500] pb-[5px]">
+                 How we use your info
+              </h2>
+              <div>
+                <h1 className="pb-[5px] text-[17px] text-[#0A1D56]">A Quick Glance</h1>
+                <ul className="list-disc list-inside pl-[15px]">
+                    <li>We may  use for personal information to improve our services</li>
+                    <li>We use a variety of security technologies and procedures to help protect your personal information from unauthorized access, use, or disclosure. For example, we store the personal information you provide on computer systems with limited access that are located in controlled facilities. When we transmit highly confidential information (such as a credit card number) over the Internet, we protect it through our Secured Payment Gateway.</li>
+                    <li>We use your personal information to communicate about promotions, upcoming events, and other news about products and services offered by us and our selected partners</li>
+                    <li>We may share personal information when we do a business deal, or negotiate a business deal, involving the sale or transfer of all or a part of our business or assets. These deals can include any merger, financing, acquisition, or bankruptcy transaction or proceeding.</li>
+                </ul>
+              </div>
+            </div>
+            <div>
+              <h2 className="text-[#0A1D56] text-[30px] font-[500] pb-[5px]">
+                 Sharing your personal info
+              </h2>
+              <div>
+              <h1 className="pb-[5px]">We may share personal information as follows:</h1>
+                <ul className="list-disc list-inside pl-[15px]">
+                  <li>We may share personal information with your consent. For example, you may let us share personal information with others for their own marketing uses. Those uses will be subject to their privacy policies</li>
+                  <li>We may share personal information for legal, protection, and safety purposes.</li>
+                  <li>We use the Order Information that we collect generally to fulfill any orders placed through the Site (including processing your payment information, arranging for shipping, and providing you with invoices and/or order confirmations)</li>
+                  <li>We may share information with those who need it to do work for us.</li>
+                  <li>We share your Personal Information with third parties to help us use your Personal Information, as described above. We also use Google Analytics to help us understand how our customers use the Site</li>
+                  <li>We may share personal information for legal, protection, and safety purposes</li>
+                  <li>We may share information to protect the rights and property of <span className="text-[#18B898]">ouragency</span> Technologies, our agents, customers, and others.</li>
+                  <li> We may also share your Personal Information to comply with applicable laws and regulations, to respond to a subpoena, search warrant or other lawful request for information we receive, or to otherwise protect our rights. we may also share your Personal Information to comply with applicable laws and regulations, to respond to a subpoena, search warrant or other lawful request for information we receive, or to otherwise protect our rights.</li>
+                </ul>
+              </div>
+            </div>
+            <div>
+              <h2 className="text-[#0A1D56] text-[30px] font-[500] pb-[5px]">Do not track</h2>
+              <div className="flex flex-col justify-start items-start gap-[10px]">
+                <p>
+                Please note that we do not alter our Site’s data collection and use practices when we see a Do Not Track signal from your browser.
+                </p>
+              </div>
+            </div>
+            <div>
+              <h2 className="text-[#0A1D56] text-[30px] font-[500] pb-[5px]">your rights</h2>
+              <div className="flex flex-col justify-start items-start gap-[10px]">
+                <p>
+                If you are an Indian residence, you have all rights, you have all rights to access to the personal information related to you and to ask that your personal information be corrected, updated, or deleted. If you would like to exercise this right, please contact us through the contact information below
+                </p>
+              </div>
+            </div>
+            <div>
+              <h2 className="text-[#0A1D56] text-[30px] font-[500] pb-[5px]">Data retention</h2>
+              <div className="flex flex-col justify-start items-start gap-[10px]">
+                <p>
+                  When you place an order through the Site, we will maintain your Order Information for our records unless and until you ask us to delete this information.
+                </p>
+              </div>
+            </div>
+            <div>
+              <h2 className="text-[#0A1D56] text-[30px] font-[500] pb-[5px]">Security</h2>
+              <div className="flex flex-col justify-start items-start gap-[10px]">
+                <p>
+                <span className="text-[#18B898]">ouragency</span> secure you that the information you share with us is highly protected. We use a variety of security technologies and procedures to help protect your personal information from unauthorized access, use, or disclosure. For example, we store the personal information you provide on computer systems with limited access that are located in controlled facilities. When we transmit highly confidential information (such as a credit card number) over the Internet, we protect it through our Secured Payment Gateway.
+                </p>
+              </div>
+            </div>
+            <div>
+              <h2 className="text-[#0A1D56] text-[30px] font-[500] pb-[5px]">Changes</h2>
+              <div className="flex flex-col justify-start items-start gap-[10px]">
+                <p>
+                We may update this privacy policy from time to time in order to reflect, for example, changes to our practices or for other operational, legal or regulatory reasons.
+                </p>
+              </div>
+            </div>
+            <div>
+              <h2 className="text-[#0A1D56] text-[30px] font-[500] pb-[5px]">Contact Us</h2>
+              <div className="flex flex-col justify-start items-start gap-[10px]">
+                <p>
+                For more information about our privacy practices, if you have questions, or if you would like to make a complaint, please contact us by e-mail at <span className="text-[#18B898]"> info@ouragency.com</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-[100%]">
+         <Footer/>
+      </div>
+    </div>
+  );
+}
---
+++ src/web/GlobalComponents/Terms.jsx
@@ -0,0 +1,116 @@
+import React from 'react'
+import Footer from './Footer'
+export default function Terms() {
+  return (
+    <div className="w-[100%] flex flex-col justify-center items-start">
+        <div className="w-[100%] relative flex justify-center items-center">
+        <div className="w-[100%] absolute top-0 left-0 z-[10]">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#F3F3F6" fill-opacity="1" d="M0,320L48,314.7C96,309,192,299,288,293.3C384,288,480,288,576,250.7C672,213,768,139,864,144C960,149,1056,235,1152,256C1248,277,1344,235,1392,213.3L1440,192L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+        </div>
+        <div className="w-[85%] relative z-[100] pt-[20px] md:pt-[25px] lg:pt-[80px]">
+          <div>
+            <h2 className="text-[#18B898] text-[20px]">Our Agency</h2>
+            <h1 className="text-[25px] text-[#FF4935]">Terms of Services</h1>
+          </div>
+          <div className="pt-[30px] flex flex-col justify-start items-start gap-[25px] md:pt-[45px]">
+            <div>
+              <div className="flex flex-col justify-start items-start gap-[10px]">
+                <p>
+                 This Services Agreement (the “Agreement”) contains the complete terms and conditions which govern your subscription of Website Design, Development and other Internet-related services provided by JS-Solutions Networks.
+                </p>
+                <p>
+                  All Graphic Design, Web Design & Development and Ongoing services provided by A.S. Designs & Services are subject to the following terms and Services.
+                </p>
+              </div>
+            </div>
+            <div>
+              <h2 className="text-[#0A1D56] text-[30px] font-[500] pb-[5px]">Estimates & Contracts</h2>
+              <div className="flex flex-col justify-start items-start gap-[10px]">
+                <p>
+                  Estimates provided to prospects will be honored for 30 days. A new quote will then be provided upon request. Contracts that have been written, but not signed by the client will only be accepted within 30 days of the delivery date. A new contract may be drawn up upon request.
+                </p>
+              </div>
+            </div>
+            <div>
+              <h2 className="text-[#0A1D56] text-[30px] font-[500] pb-[5px]">Non-Disclosure</h2>
+              <div className="flex flex-col justify-start items-start gap-[10px]">
+                <p>
+                  We (and any subcontractors/ re-sellers we engage) agree that we will not at any time disclose any of your confidential information to any third party.
+                </p>
+              </div>
+            </div>
+            <div>
+              <h2 className="text-[#0A1D56] text-[30px] font-[500] pb-[5px]">Data retention</h2>
+              <div className="flex flex-col justify-start items-start gap-[10px]">
+                <p>
+                  When you place an order through the Site, we will maintain your Order Information for our records unless and until you ask us to delete this information.
+                </p>
+              </div>
+            </div>
+            <div>
+              <h2 className="text-[#0A1D56] text-[30px] font-[500] pb-[5px]">Client</h2>
+              <div className="flex flex-col justify-start items-start gap-[10px]">
+                <p>
+                “Client” is defined as representing one business (company or entity) or person (sole trader). Client may also be referred to as “himself/herself” or “his/her” or “their” or “site owner” in this document. Client may also be referred to as the staff member, employee, agent or third party on behalf the Client.
+                </p>
+              </div>
+            </div>
+            <div>
+              <h2 className="text-[#0A1D56] text-[30px] font-[500] pb-[5px]">Variations</h2>
+              <div className="flex flex-col justify-start items-start gap-[10px]">
+                <p>
+                We are pleased to offer you the opportunity to make revisions to the design. However, we have the right to limit the number of design proposals to a reasonable amount and may charge for additional designs if you make a change to the original design specification.
+                </p>
+                <p>Our website development phase is flexible and allows certain variations to the original specification. However any major deviation from the specification will be charged as per the variations.</p>
+              </div>
+            </div>
+            <div>
+              <h2 className="text-[#0A1D56] text-[30px] font-[500] pb-[5px]">Fess Deposits</h2>
+              <div className="flex flex-col justify-start items-start gap-[10px]">
+                <p>
+                A 50% deposit of the total fee payable under our proposal is due immediately upon you instructing us to proceed with the website design and development work. The remaining 50% shall become due when the work is completed to your reasonable satisfaction. We reserve the right not to commence any work until the deposit has been paid in full.
+                </p>
+                <p>The 50% deposit of advance paid is NON refundable as we book the domain & hosting on customer behalf. (We collect 50% payment after booking the domain & hosting)</p>
+              </div>
+            </div>
+            <div>
+              <h2 className="text-[#0A1D56] text-[30px] font-[500] pb-[5px]">Deliverables</h2>
+              <div className="flex flex-col justify-start items-start gap-[10px]">
+                <p>
+                Any time frames or estimates that we give are contingent upon your full co-operation and complete and final content in photography for the work pages. During development there is a certain amount of feedback required in order to progress to subsequent phases. It is required that a single point of contact be appointed from your side and be made available on a daily basis in order to expedite the feedback process.
+                </p>
+              </div>
+            </div>
+            <div>
+              <h2 className="text-[#0A1D56] text-[30px] font-[500] pb-[5px]">Payment</h2>
+              <div className="flex flex-col justify-start items-start gap-[10px]">
+                <p>
+                  For each project contract, there are payment milestones that will occur at various stages or points during the timeline of project. For each payment milestone, a fee is due as set out per contract which will be invoiced at the time of the payment milestone being reached with payment by the Client on that invoice due immediately unless otherwise agreed to in writing between the parties.
+                </p>
+              </div>
+            </div>
+            <div>
+              <h2 className="text-[#0A1D56] text-[30px] font-[500] pb-[5px]">E-commers Plan</h2>
+              <div className="flex flex-col justify-start items-start gap-[10px]">
+                <p>
+                You are responsible for complying with all relevant laws relating to e-commerce, and to the full extent permitted by law will hold harmless, protect, and defend and indemnify Standsweb and its subcontractors from any claim, penalty, tax, tariff loss or damage arising from your or your clients’ use of Internet electronic commerce.
+                </p>
+              </div>
+            </div>
+            <div>
+              <h2 className="text-[#0A1D56] text-[30px] font-[500] pb-[5px]">Policy updates</h2>
+              <div className="flex flex-col justify-start items-start gap-[10px]">
+                <p>
+                  Standsweb is permitted to revise these Terms of conditions/ services at any time as it sees fit, the orders taken prior to update of terms of conditions will also come under new terms of conditions (If you think the changes made will affect your projects, you need to update us within 10 days of terms of conditions being updated). And by using this Website you are expected to review such Terms on a regular basis to ensure you understand all terms and conditions governing use of this Website.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-[100%]">
+         <Footer/>
+      </div>
+    </div>
+  )
+}
